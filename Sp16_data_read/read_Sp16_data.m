8ff06ee607f675d08ac8c1ee5eff3e5dc5e59029
Zainab A. Hussein <abzain.hussein@outlook.com>
2017-03-08 12:24:27 -0500

    sp16_data_xls_to_matlab
    read by load value

---
+++ Sp16_data_read/read_Sp16_data.m
@@ -0,0 +1,4 @@
+%% read excel file into matlab
+% differentiate by load values
+% syntax: num = xlsread( filename, x1Range )
+% https://www.mathworks.com/help/matlab/ref/xlsread.html?requestedDomain=www.mathworks.com
